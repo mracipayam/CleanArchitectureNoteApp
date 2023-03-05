7b73d26315b22482a7037f302c975ebbfa3b3c73
Murat Han Acıpayam <acipayammurathan@gmail.com>
2023-03-05 22:59:55 +0300

    Update OrderSection.kt

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/OrderSection.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/components/OrderSection.kt
@@ -3,6 +3,7 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.co
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
@@ -15,6 +16,12 @@ fun OrderSection(
 ){
     Column(modifier = modifier) {
         Row(modifier = Modifier.fillMaxWidth()) {
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
             DefaultRadioButton(
                 text = "Title",
                 selected = noteOrder is NoteOrder.Title,
